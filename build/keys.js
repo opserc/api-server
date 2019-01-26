c407ca84e3a2777fef4823c830b1cb2eff765521
Opserc <jesusecrespo@gmail.com>
2019-01-26 17:13:36 -0400

    se genero el /build

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'coop'
+        host: 'mysql.zz.com.ve',
+        user: 'opserc',
+        password: 'Madrid17',
+        database: 'opsercj'
     }
 };
